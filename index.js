82f15452293bd9a475745bec83eedf26274e3212
baiel nasirdinuulu <baiel.nasirdin.alt@gmail.com>
2022-05-23 20:54:41 +0600

    re

---
+++ index.js
@@ -0,0 +1,46 @@
+// homework 1
+
+let enterTime = prompt("enterTime ");
+let seconds = 3600;
+
+document.write(enterTime * seconds);
+
+// homework 2
+
+let myNumber = 21;
+alert(myNumber);
+
+// homework 3
+
+let name = prompt("enter your name");
+
+document.write(`Рад знакомству ${name}!`);
+
+// homework 4
+
+let a = parseInt(prompt("enter your number"));
+let b = parseInt(prompt("enter your number"));
+
+a += b;
+document.write(a / 2);
+
+//homework 5
+
+let number = parseInt(prompt("Enter your number"));
+
+document.write(number + 1);
+
+//homework 6
+
+const user = {
+  name: "baiel",
+  age: 21,
+  friend: "aibek",
+  email: "baiel.nasirdin.alt@gmail.com",
+};
+const user2 = {
+  name: "baiel",
+  age: 21,
+  friend: "aibek",
+  email: "baiel.nasirdin.alt@gmail.com",
+};
