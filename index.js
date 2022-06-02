fc15fd184fde3dcc65c8159cbe9377389544e5b7
baiel nasirdinuulu <baiel.nasirdin.alt@gmail.com>
2022-06-02 18:21:23 +0600

    addhomework

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // homework 1
-
+/*
 let enterTime = prompt("enterTime ");
 let seconds = 3600;
 
@@ -44,3 +44,40 @@ const user2 = {
   friend: "aibek",
   email: "baiel.nasirdin.alt@gmail.com",
 };
+*/
+
+//homework 7
+
+let post = {
+  postName: "heart",
+  authorPost: "baiel",
+  datePost: 23,
+  bodyPost: 54,
+  linkPOst:
+    "https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80",
+};
+
+console.log(post);
+
+//homework 8
+
+let user4 = ["baiel", "aibek", "syimyk"];
+console.log(user4[2]);
+
+//homework 9
+let user5 = {
+  name: "https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80",
+};
+console.log(user5);
+
+let a = [
+  {
+    postName: "heart",
+    authorPost: "baiel",
+    datePost: 23,
+    bodyPost: 54,
+    linkPOst:
+      "https://images.unsplash.com/photo-1566275529824-cca6d008f3da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cGhvdG98ZW58MHx8MHx8&w=1000&q=80",
+  },
+];
+console.log(a);
